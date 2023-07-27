d57e16cf4ef736910d795ab80466909ec22841e3
Guilherme <guilherme.beutler@outlook.com>
2023-07-27 15:21:54 -0300

    feat: create home page

---
+++ src/app/portfolio/[category]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PortfolioCategory() {
+	return (
+		<div>
+			<h1>PortfolioCategory</h1>
+		</div>
+	)
+}
+
+export default PortfolioCategory
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,64 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+import styles from './navbar.module.css'
+
+const links = [
+	{
+		id: 1,
+		title: 'Home',
+		url: '/',
+	},
+	{
+		id: 2,
+		title: 'Portfolio',
+		url: '/portfolio',
+	},
+	{
+		id: 3,
+		title: 'Blog',
+		url: '/blog',
+	},
+	{
+		id: 4,
+		title: 'About',
+		url: '/about',
+	},
+	{
+		id: 5,
+		title: 'Contact',
+		url: '/contact',
+	},
+	{
+		id: 6,
+		title: 'Dashboard',
+		url: '/dashboard',
+	},
+]
+
+function Navbar() {
+	return (
+		<div className={styles.container}>
+			<Link className={styles.logo} href="/">
+				Lamia
+			</Link>
+			<div className={styles.links}>
+				{links.map((link) => (
+					<Link key={link.id} href={link.url} className="link">
+						{link.title}
+					</Link>
+				))}
+				<button
+					className={styles.button}
+					onClick={() => {
+						console.log('logged out')
+					}}
+				>
+					Logout
+				</button>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
