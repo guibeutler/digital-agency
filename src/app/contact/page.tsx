7dc3f1a3c17a8e183463aa5b1ed329304863ea5a
Guilherme <guilherme.beutler@outlook.com>
2023-07-28 11:37:49 -0300

    feat: contact page created

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Image from 'next/image'
+import Button from '@components/Button'
+import styles from './page.module.css'
 
 function Contact() {
 	return (
-		<div>
-			<h1>Contact</h1>
+		<div className={styles.container}>
+			<h1 className={styles.title}>Let's Keep in Touch</h1>
+			<div className={styles.content}>
+				<div className={styles.imgContainer}>
+					<Image className={styles.img} src="/contact.png" alt="" fill={true} />
+				</div>
+				<form className={styles.form}>
+					<input type="text" placeholder="name" className={styles.input} />
+					<input type="text" placeholder="email" className={styles.input} />
+					<textarea
+						placeholder="message"
+						className={styles.textArea}
+						cols={30}
+						rows={10}
+					/>
+					<Button text="Send" url="#" />
+				</form>
+			</div>
 		</div>
 	)
 }
